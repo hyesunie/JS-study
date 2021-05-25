876d77eb642380f19c0641bfce70c46d6de6b691
hyesunie <hyesunie1025@gmail.com>
2021-05-25 21:21:43 +0900

    Study Symbol

--- object5.js
+++ object5.js
@@ -1,8 +1,7 @@
-let user = null;
-let x = 0;
+//optional chainning
+let user = {}; // 주소 정보가 없는 사용자
 
-user?.sayHi(x++);
-console.log(x);
+console.log(user?.address?.street);
 
 //함수가 있는지 확인할 떄
 
@@ -16,3 +15,16 @@ let user2 = {};
 
 user1.admin?.();
 user2.admin?.();
+
+//symbol
+let id = Symbol("id");
+let user = {
+  name: "John",
+  age: 30,
+  [id]: 123,
+};
+
+for (let key in user) alert(key); // name과 age만 출력되고, 심볼은 출력되지 않습니다.
+
+// 심볼로 직접 접근하면 잘 작동합니다.
+alert("직접 접근한 값: " + user[id]);
